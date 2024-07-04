3b3b91624440813f3f6c30dcac1d8b66d89bcaf7
JSebastianRLA <joan.rojas@ngeek.co>
2024-07-04 11:33:48 -0500

    Cambios de la extencion

--- js/background.js
+++ js/background.js
@@ -5,7 +5,7 @@ const maxEventosAlmacenados = 10; // Definir el máximo de eventos almacenados
 
 // Obtener las credenciales almacenadas del almacenamiento local al inicio
 chrome.storage.local.get(["credentials"], function (items) {
-  credentials = items.credentials;
+    credentials = items.credentials;
 });
 
 // No es necesario verificar las credenciales cada vez que se abre la extensión
@@ -13,111 +13,111 @@ chrome.storage.local.get(["credentials"], function (items) {
 
 // Escuchar mensajes del script de contenido
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action === "saveCredentials") {
-    // Almacenar las credenciales proporcionadas por el usuario
-    credentials = request.credentials;
+    if (request.action === "saveCredentials") {
+        // Almacenar las credenciales proporcionadas por el usuario
+        credentials = request.credentials;
 
-    // Guardar las credenciales en el almacenamiento local para su uso futuro
-    chrome.storage.local.set({ credentials: credentials });
+        // Guardar las credenciales en el almacenamiento local para su uso futuro
+        chrome.storage.local.set({ credentials: credentials });
 
-    sendResponse({ success: true }); // Confirmar que las credenciales se guardaron correctamente
-  }
+        sendResponse({ success: true }); // Confirmar que las credenciales se guardaron correctamente
+    }
 });
 
 // Definir la función para verificar cambios en la API
 function checkAPIForChanges() {
-  if (!credentials) {
-    console.log("Faltan credenciales para realizar la consulta a la API.");
-    return;
-  }
+    if (!credentials) {
+        console.log("Faltan credenciales para realizar la consulta a la API.");
+        return;
+    }
 
-  // Construir la URL de la API utilizando las credenciales almacenadas
-  const { dns, apiPass, username, password } = credentials;
-  const apiUrl = `https://${dns}/pandora_console/include/api.php?op=get&op2=events&return_type=json&apipass=${apiPass}&user=${username}&pass=${password}`;
-  console.log(apiUrl);
-  // Obtener los datos de la API
-  fetch(apiUrl)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("La solicitud a la API falló: " + response.statusText);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-      const nuevosEventos = data.data;
-      console.log("Eventos nuevos recibidos:", nuevosEventos.length);
-
-      // Filtrar los eventos que no estén en la lista de eventos almacenados
-      const eventosNuevos = nuevosEventos.filter((evento) => {
-        return !eventosAnteriores.some(
-          (eventoAnterior) => eventoAnterior.id_evento === evento.id_evento
-        );
-      });
-
-      console.log(data.data[0].event_type);
-      console.log("Eventos nuevos filtrados:", eventosNuevos);
-
-      // Si hay eventos nuevos, mostrar una notificación
-      if (eventosNuevos.length > 0) {
-        eventosNuevos.forEach((evento) => {
-          if (
-            evento.event_type === "going_up_critical" ||
-            evento.event_type === "going_down_critical" ||
-            evento.event_type === "going_down_warning" ||
-            evento.event_type === "going_up_warning"
-          ) {
-            console.log(evento.event_type);
-            let mensaje = "";
-
-            switch (evento.event_type) {
-              case "going_up_critical":
-                mensaje = "Hay un nuevo evento crítico.";
-                // audioCritico.play();
-                break;
-              case "going_down_critical":
-                mensaje = "Hay un nuevo evento crítico.";
-                // audioCritico.play();
-                break;
-              case "going_down_warning":
-                mensaje = "Hay un nuevo evento peligroso.";
-                // audioPeligroso.play();
-                break;
-              case "going_up_warning":
-                mensaje = "Hay un nuevo evento peligroso.";
-                // audioPeligroso.play();
-                break;
+    // Construir la URL de la API utilizando las credenciales almacenadas
+    const { dns, apiPass, username, password } = credentials;
+    const apiUrl = `https://${dns}/pandora_console/include/api.php?op=get&op2=events&return_type=json&apipass=${apiPass}&user=${username}&pass=${password}`;
+    console.log(apiUrl);
+    // Obtener los datos de la API
+    fetch(apiUrl)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("La solicitud a la API falló: " + response.statusText);
             }
-            const fechaHoraActual = new Date().toLocaleString();
-            chrome.notifications.create(null, {
-              type: "basic",
-              iconUrl: "images/icon128.png",
-              title: "¡Nuevo evento!",
-              message: `${mensaje}, en el agente con ID ${evento.id_agente}, con fecha y hora: ${fechaHoraActual}`,
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            const nuevosEventos = data.data;
+            console.log("Eventos nuevos recibidos:", nuevosEventos.length);
+
+            // Filtrar los eventos que no estén en la lista de eventos almacenados
+            const eventosNuevos = nuevosEventos.filter((evento) => {
+                return !eventosAnteriores.some(
+                    (eventoAnterior) => eventoAnterior.id_evento === evento.id_evento
+                );
             });
-          }
-          console.log("Evento normal");
+
+            console.log(data.data[0].event_type);
+            console.log("Eventos nuevos filtrados:", eventosNuevos);
+
+            // Si hay eventos nuevos, mostrar una notificación
+            if (eventosNuevos.length > 0) {
+                eventosNuevos.forEach((evento) => {
+                    if (
+                        evento.event_type === "going_up_critical" ||
+                        evento.event_type === "going_down_critical" ||
+                        evento.event_type === "going_down_warning" ||
+                        evento.event_type === "going_up_warning"
+                    ) {
+                        console.log(evento.event_type);
+                        let mensaje = "";
+
+                        switch (evento.event_type) {
+                            case "going_up_critical":
+                                mensaje = "Hay un nuevo evento crítico.";
+                                // audioCritico.play();
+                                break;
+                            case "going_down_critical":
+                                mensaje = "Hay un nuevo evento crítico.";
+                                // audioCritico.play();
+                                break;
+                            case "going_down_warning":
+                                mensaje = "Hay un nuevo evento peligroso.";
+                                // audioPeligroso.play();
+                                break;
+                            case "going_up_warning":
+                                mensaje = "Hay un nuevo evento peligroso.";
+                                // audioPeligroso.play();
+                                break;
+                        }
+                        const fechaHoraActual = new Date().toLocaleString();
+                        chrome.notifications.create(null, {
+                            type: "basic",
+                            iconUrl: "images/icon128.png",
+                            title: "¡Nuevo evento!",
+                            message: `${mensaje}, en el agente con ID ${evento.id_agente}, con fecha y hora: ${fechaHoraActual}`,
+                        });
+                    }
+                    console.log("Evento normal");
+                });
+            }
+
+            // Actualizar los eventos almacenados
+            eventosAnteriores = nuevosEventos;
+
+            // Enviar la data al popup
+            chrome.runtime.sendMessage({
+                action: "apiData",
+                eventos: nuevosEventos,
+            });
+        })
+        .catch((error) => {
+            console.error("Error al obtener datos de la API:", error);
         });
-      }
-
-      // Actualizar los eventos almacenados
-      eventosAnteriores = nuevosEventos;
-
-      // Enviar la data al popup
-      chrome.runtime.sendMessage({
-        action: "apiData",
-        eventos: nuevosEventos,
-      });
-    })
-    .catch((error) => {
-      console.error("Error al obtener datos de la API:", error);
-    });
-  // Si no hay eventos nuevos, mostrar el mensaje "Sin eventos nuevos"
-  if (eventosNuevos.length === 0) {
-    document.getElementById("noEventsMessage").style.display = "block";
-  } else {
-    document.getElementById("noEventsMessage").style.display = "none";
-  }
+    // Si no hay eventos nuevos, mostrar el mensaje "Sin eventos nuevos"
+    if (eventosNuevos.length === 0) {
+        document.getElementById("noEventsMessage").style.display = "block";
+    } else {
+        document.getElementById("noEventsMessage").style.display = "none";
+    }
 }
 
 // Establecer intervalo para llamar a la función cada 10 segundos
--- js/popup.js
+++ js/popup.js
@@ -2,196 +2,196 @@
 let dns, apiPass, username, password;
 
 document.addEventListener("DOMContentLoaded", function () {
-  const buttonContainer = document.getElementById("buttonContainer");
-  document
-    .getElementById("toggleButton")
-    .addEventListener("click", function () {
-      const formContainer = document.getElementById("formContainer");
-      formContainer.style.display =
-        formContainer.style.display === "none" ? "block" : "none";
-    });
-  const formContainer = document.getElementById("formContainer");
-  formContainer.style.display = "none";
+    const buttonContainer = document.getElementById("buttonContainer");
+    document
+        .getElementById("toggleButton")
+        .addEventListener("click", function () {
+            const formContainer = document.getElementById("formContainer");
+            formContainer.style.display =
+                formContainer.style.display === "none" ? "block" : "none";
+        });
+    const formContainer = document.getElementById("formContainer");
+    formContainer.style.display = "none";
 
-  const checkButton = document.getElementById("checkButton");
-  const spinnerContainer = document.getElementById("loadingIndicator"); // Cambiado el ID del contenedor del spinner
+    const checkButton = document.getElementById("checkButton");
+    const spinnerContainer = document.getElementById("loadingIndicator"); // Cambiado el ID del contenedor del spinner
 
-  // Función para guardar las credenciales en el almacenamiento local
-  function saveCredentials() {
-    dns = document.getElementById("dns").value;
-    apiPass = document.getElementById("apiPass").value;
-    username = document.getElementById("username").value;
-    password = document.getElementById("password").value;
+    // Función para guardar las credenciales en el almacenamiento local
+    function saveCredentials() {
+        dns = document.getElementById("dns").value;
+        apiPass = document.getElementById("apiPass").value;
+        username = document.getElementById("username").value;
+        password = document.getElementById("password").value;
 
-    // Almacenar las credenciales en el almacenamiento local
-    chrome.storage.local.set({
-      dns: dns,
-      apiPass: apiPass,
-      username: username,
-      password: password,
-    });
-  }
-
-  // Recuperar las credenciales almacenadas del almacenamiento local y establecerlas en los campos de entrada
-  chrome.storage.local.get(
-    ["dns", "apiPass", "username", "password"],
-    function (items) {
-      dns = items.dns || "";
-      apiPass = items.apiPass || "";
-      username = items.username || "";
-      password = items.password || "";
-
-      document.getElementById("dns").value = dns;
-      document.getElementById("apiPass").value = apiPass;
-      document.getElementById("username").value = username;
-      document.getElementById("password").value = password;
+        // Almacenar las credenciales en el almacenamiento local
+        chrome.storage.local.set({
+            dns: dns,
+            apiPass: apiPass,
+            username: username,
+            password: password,
+        });
     }
-  );
 
-  checkButton.addEventListener("click", function () {
-    // Mostrar el spinner
-    spinnerContainer.style.display = "block";
+    // Recuperar las credenciales almacenadas del almacenamiento local y establecerlas en los campos de entrada
+    chrome.storage.local.get(
+        ["dns", "apiPass", "username", "password"],
+        function (items) {
+            dns = items.dns || "";
+            apiPass = items.apiPass || "";
+            username = items.username || "";
+            password = items.password || "";
 
-    // Guardar las credenciales antes de realizar la verificación de la API
-    saveCredentials();
-
-    const resultDiv = document.getElementById("eventTableBody");
-    resultDiv.innerText = "Verificando API...";
-
-    // Envía un mensaje al script de fondo con los valores de los campos de entrada
-    chrome.runtime.sendMessage(
-      {
-        action: "saveCredentials",
-        credentials: {
-          dns: dns,
-          apiPass: apiPass,
-          username: username,
-          password: password,
-        },
-      },
-      function (response) {
-        if (response && response.success) {
-          resultDiv.innerText = "¡API verificada con éxito!";
-        } else {
-          resultDiv.innerText = "Error al verificar la API.";
+            document.getElementById("dns").value = dns;
+            document.getElementById("apiPass").value = apiPass;
+            document.getElementById("username").value = username;
+            document.getElementById("password").value = password;
         }
-      }
     );
 
-    // Ocultar el spinner después de 3 segundos
-    setTimeout(function () {
-      spinnerContainer.style.display = "none";
-    }, 3000);
-  });
+    checkButton.addEventListener("click", function () {
+        // Mostrar el spinner
+        spinnerContainer.style.display = "block";
 
-  // Función para imprimir un mensaje con el ID del evento
-  function imprimirMensaje(idEvento) {
-    console.log("Hola mundo " + idEvento);
-  }
+        // Guardar las credenciales antes de realizar la verificación de la API
+        saveCredentials();
 
-  // Función para validar un evento por su ID
-  function validarEventoPorId(idEvento) {
-    // Construir la URL de la API con los parámetros necesarios
-    const url = `https://${dns}/pandora_console/include/api.php?op=set&op2=validate_event_by_id&id=${idEvento}&apipass=${apiPass}&user=${username}&pass=${password}`;
+        const resultDiv = document.getElementById("eventTableBody");
+        resultDiv.innerText = "Verificando API...";
 
-    // Realizar la solicitud HTTP utilizando fetch
-    fetch(url)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Error al validar el evento");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        // Manejar la respuesta de la API
-        console.log("Evento validado con éxito:", data);
-      })
-      .catch((error) => {
-        // Manejar errores de la solicitud
-        console.error("Error al validar el evento:", error);
-      });
-  }
+        // Envía un mensaje al script de fondo con los valores de los campos de entrada
+        chrome.runtime.sendMessage(
+            {
+                action: "saveCredentials",
+                credentials: {
+                    dns: dns,
+                    apiPass: apiPass,
+                    username: username,
+                    password: password,
+                },
+            },
+            function (response) {
+                if (response && response.success) {
+                    resultDiv.innerText = "¡API verificada con éxito!";
+                } else {
+                    resultDiv.innerText = "Error al verificar la API.";
+                }
+            }
+        );
 
-  // Escuchar mensajes del script de fondo
-  chrome.runtime.onMessage.addListener(function (
-    message,
-    sender,
-    sendResponse
-  ) {
-    if (message.action === "apiData") {
-      // Procesar los datos recibidos de la API
-      const eventData = message.eventos;
-
-      // Filtrar los eventos según las condiciones especificadas
-      const filteredEvents = eventData.filter((event) => {
-        const event_type = event.evento;
-        const estado = event.estado;
-        let estadClass = "";
-        let iconType = "";
-        if (
-          event.event_type === "going_up_warning" ||
-          event.event_type === "going_down_warning"
-        ) {
-          iconType = "fa-solid fa-triangle-exclamation";
-          estadClass = "estadoYe";
-        } else if (
-          event.event_type === "going_down_critical" ||
-          event.event_type === "going_up_critical"
-        ) {
-          iconType = "fa-solid fa-circle-exclamation";
-          estadClass = "estadoRe";
-        }
-        return estadClass && event.estado === "0";
-      });
-
-      // Construir la tabla HTML con los eventos filtrados
-      let tableHTML = "";
-
-      // Declarar iconType antes del ciclo
-      let iconType = "";
-      // Declarar estadClass antes del ciclo
-      let estadClass = "";
-
-      for (const event of filteredEvents) {
-        estadClass =
-          event.event_type === "going_up_warning" ||
-          event.event_type === "going_down_warning"
-            ? "estadoYe"
-            : "estadoRe";
-        iconType =
-          event.event_type === "going_up_warning" ||
-          event.event_type === "going_down_warning"
-            ? "fa-solid fa-triangle-exclamation"
-            : "fa-solid fa-circle-exclamation";
-
-        tableHTML += `<tr><td><div class='${estadClass}'><i class='${iconType}'></i></div></td><td>${event.evento}</td><td>${event.id_agente}</td>`;
-        // Crear el nuevo botón y agregar el evento de clic
-        tableHTML += `<td><div class="BtnVal"><button title="Enviar validación" alt="Validar" class="new-button" data-id="${event.id_evento}"><i class="fa-solid fa-square-check"></i></button></div></td></tr>`;
-      }
-      // Agregar las filas a la tabla existente
-      const resultTableBody = document.getElementById("eventTableBody");
-      resultTableBody.innerHTML = tableHTML;
-
-      // Escuchar clics en los botones con la clase "new-button"
-      const newButtons = document.querySelectorAll(".new-button");
-      newButtons.forEach((button) => {
-        button.addEventListener("click", function () {
-          const eventId = this.getAttribute("data-id");
-          console.log("ID del evento:", eventId);
-          imprimirMensaje(eventId);
-
-          // Mostrar el spinner
-          spinnerContainer.style.display = "block";
-
-          // Ocultar el spinner después de 3 segundos
-          setTimeout(function () {
+        // Ocultar el spinner después de 3 segundos
+        setTimeout(function () {
             spinnerContainer.style.display = "none";
-          }, 3000);
+        }, 3000);
+    });
 
-          // Validar el evento por su ID
-          validarEventoPorId(eventId);
-        });
-      });
+    // Función para imprimir un mensaje con el ID del evento
+    function imprimirMensaje(idEvento) {
+        console.log("Hola mundo " + idEvento);
     }
-  });
+
+    // Función para validar un evento por su ID
+    function validarEventoPorId(idEvento) {
+        // Construir la URL de la API con los parámetros necesarios
+        const url = `https://${dns}/pandora_console/include/api.php?op=set&op2=validate_event_by_id&id=${idEvento}&apipass=${apiPass}&user=${username}&pass=${password}`;
+
+        // Realizar la solicitud HTTP utilizando fetch
+        fetch(url)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Error al validar el evento");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                // Manejar la respuesta de la API
+                console.log("Evento validado con éxito:", data);
+            })
+            .catch((error) => {
+                // Manejar errores de la solicitud
+                console.error("Error al validar el evento:", error);
+            });
+    }
+
+    // Escuchar mensajes del script de fondo
+    chrome.runtime.onMessage.addListener(function (
+        message,
+        sender,
+        sendResponse
+    ) {
+        if (message.action === "apiData") {
+            // Procesar los datos recibidos de la API
+            const eventData = message.eventos;
+
+            // Filtrar los eventos según las condiciones especificadas
+            const filteredEvents = eventData.filter((event) => {
+                const event_type = event.evento;
+                const estado = event.estado;
+                let estadClass = "";
+                let iconType = "";
+                if (
+                    event.event_type === "going_up_warning" ||
+                    event.event_type === "going_down_warning"
+                ) {
+                    iconType = "fa-solid fa-triangle-exclamation";
+                    estadClass = "estadoYe";
+                } else if (
+                    event.event_type === "going_down_critical" ||
+                    event.event_type === "going_up_critical"
+                ) {
+                    iconType = "fa-solid fa-circle-exclamation";
+                    estadClass = "estadoRe";
+                }
+                return estadClass && event.estado === "0";
+            });
+
+            // Construir la tabla HTML con los eventos filtrados
+            let tableHTML = "";
+
+            // Declarar iconType antes del ciclo
+            let iconType = "";
+            // Declarar estadClass antes del ciclo
+            let estadClass = "";
+
+            for (const event of filteredEvents) {
+                estadClass =
+                    event.event_type === "going_up_warning" ||
+                        event.event_type === "going_down_warning"
+                        ? "estadoYe"
+                        : "estadoRe";
+                iconType =
+                    event.event_type === "going_up_warning" ||
+                        event.event_type === "going_down_warning"
+                        ? "fa-solid fa-triangle-exclamation"
+                        : "fa-solid fa-circle-exclamation";
+
+                tableHTML += `<tr><td><div class='${estadClass}'><i class='${iconType}'></i></div></td><td>${event.evento}</td><td>${event.id_agente}</td>`;
+                // Crear el nuevo botón y agregar el evento de clic
+                tableHTML += `<td><div class="BtnVal"><button title="Enviar validación" alt="Validar" class="new-button" data-id="${event.id_evento}"><i class="fa-solid fa-square-check"></i></button></div></td></tr>`;
+            }
+            // Agregar las filas a la tabla existente
+            const resultTableBody = document.getElementById("eventTableBody");
+            resultTableBody.innerHTML = tableHTML;
+
+            // Escuchar clics en los botones con la clase "new-button"
+            const newButtons = document.querySelectorAll(".new-button");
+            newButtons.forEach((button) => {
+                button.addEventListener("click", function () {
+                    const eventId = this.getAttribute("data-id");
+                    console.log("ID del evento:", eventId);
+                    imprimirMensaje(eventId);
+
+                    // Mostrar el spinner
+                    spinnerContainer.style.display = "block";
+
+                    // Ocultar el spinner después de 3 segundos
+                    setTimeout(function () {
+                        spinnerContainer.style.display = "none";
+                    }, 3000);
+
+                    // Validar el evento por su ID
+                    validarEventoPorId(eventId);
+                });
+            });
+        }
+    });
 });
